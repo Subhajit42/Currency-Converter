c0be22e6af979228cb597df5bab4c958fc5e9a48
subhajit1742@gmail.com <subhajit1742@gmail.com>
2023-03-21 00:50:49 +0530

    2 state variables added

--- src/Components/InputCurrency.js
+++ src/Components/InputCurrency.js
@@ -1,6 +1,21 @@
-import React from 'react'
+import React , {useState} from 'react'
 
 export default function InputCurrency() {
+  const inpValueCurr = (event) => {
+      setVal(event.target.value);   
+  }
+
+  const outValueCurr = () =>{
+      setVal2(val*2);
+  }
+
+  const print = () =>{
+    console.log("hi");
+  }
+
+const [val,setVal] = useState(0);
+const [val2,setVal2] = useState(0);
+
   return (
     <>
 
@@ -10,15 +25,16 @@ export default function InputCurrency() {
         </div>
         <select className="custom-select" id="inputGroupSelect01">
             <option defaultValue>--Choose--</option>
-            <option value="1">Rupee</option>
+            <option value="1" addActivityItem={print} >Rupee</option>
             <option value="2">Dollar</option>
         </select>
     </div>
 
     {/* INPUT for currency to be changed */}
     <div className="inputCurrency">
-        <input type="number" className="inputCurr mx-4"/>
-        <button type="button" className="btn btn-success">Convert</button>
+        <input type="number" className="inputCurr mx-4" onChange={inpValueCurr} value={val} />
+        <button type="button" className="btn btn-success" onClick={outValueCurr} >Convert</button>
+        <button type="button" className="btn btn-primary" >Convert</button>
     </div>
     
     
@@ -36,8 +52,9 @@ export default function InputCurrency() {
 
 
     {/* OUTPUT Currency */}
-    <div className="outputCurrency">
-
+    <div className="outputCurrency-border mx-4">
+      <textarea rows={1} cols={23} value={val2}></textarea>
+      {/* {val2} */}
     </div>
     </>
   )
